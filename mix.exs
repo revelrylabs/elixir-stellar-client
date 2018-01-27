e0892c1dbc4d523d5c56201ced0dd4838964ef8f
Bryan Joseph <bryanjos@gmail.com>
2018-01-27 14:52:24 -0600

    Use release version of jason

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule Stellar.MixProject do
   defp deps do
     [
       {:httpoison, "~> 1.0"},
-      {:jason, "~> 1.0-rc"},
+      {:jason, "~> 1.0"},
       {:ex_doc, "~> 0.18.1", only: :dev},
       {:bypass, "~> 0.8.1", only: :test},
       {:excoveralls, "~> 0.8.0", only: :test}
