a37e518acca499541d12c7b8981a5f4f438dd142
Bryan Joseph <bryanjos@gmail.com>
2018-03-12 19:40:16 -0500

    Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Stellar.MixProject do
   def project do
     [
       app: :stellar,
-      version: "0.2.0",
+      version: "0.2.1",
       description: description(),
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
