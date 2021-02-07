f0b83375bb429afef5497c9b407e2f9eb97e28f7
Luke Ledet <luke@revelry.co>
2021-02-06 18:06:47 -0600

    ,

--- config/test.exs
+++ config/test.exs
@@ -4,7 +4,7 @@ config :stellar, network: :test
 
 # This account ONLY exists on the testnet and is ONLY used in the test suite
 config :stellar, :test_account,
-  public_key: "GAW7DAQWCZS7OKF3KTLDNW3BBRUQOQKMOGPM33SNV5XW6FYVQ2URI7B4"
+  public_key: "GAW7DAQWCZS7OKF3KTLDNW3BBRUQOQKMOGPM33SNV5XW6FYVQ2URI7B4",
   secret: "SCTQEYOV2FBROO76J2CRBBMJQVNL67OLID74JFC4TSI6QAQ6JLUUFLLA"
 
 if File.exists?(Path.join([__DIR__, "test.secret.exs"])) do
